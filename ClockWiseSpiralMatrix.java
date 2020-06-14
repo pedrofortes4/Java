a20cc1848ad8e22b2d74b299a4eb998ef7b7c30b
Pedro Fortes <55544345+pedrofortes4@users.noreply.github.com>
2020-06-14 03:49:36 -0300

    Update ClockWiseSpiralMatrix.java

--- ClockWiseSpiralMatrix.java
+++ ClockWiseSpiralMatrix.java
@@ -1,8 +1,8 @@
-package testes;
+package spiral;
 
 import java.util.Scanner;
 
-public class Testes {
+public class Spiral {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
